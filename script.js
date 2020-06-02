ee6dab5b5b9956097a596e95677914452543fbd0
Youzhi Hu <jyzhu0101@gmail.com>
2020-06-02 14:27:19 +1000

    Prompt password criteria when clicking the generate password button

--- script.js
+++ script.js
@@ -11,3 +11,92 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt password criteria when click generate password button
+function generatePassword() {
+    // Let user set password length
+    pwdLength = parseInt(
+        prompt("How many characters would you like your password to contain?")
+    );
+
+    if (pwdLength <= 7 || pwdLength >= 129) {
+        while (pwdLength <= 7 || pwdLength >= 129) {
+            pwdLength = parseInt(
+                prompt(
+                    "How many characters would you like your password to contain?"
+                )
+            );
+        }
+    } else if (pwdLength >= 8 && pwdLength <= 128) {
+        alert("Your password length is valid. Please continue!");
+    }
+
+    console.log(pwdLength);
+
+    // Let user confirm using special characters
+    splChar = confirm("Click OK to confirm including special character.");
+
+    if (splChar == false) {
+        while (splChar == false) {
+            splChar = confirm(
+                "Click OK to confirm including special characters."
+            );
+        }
+    }
+    // console.log(splChar);
+
+    // Let user confirm using numeric characters
+    numChar = confirm("Click OK to confirm including numeric character.");
+
+    if (numChar == false) {
+        while (numChar == false) {
+            numChar = confirm(
+                "Click OK to confirm including numeric characters."
+            );
+        }
+    }
+    // console.log(numChar);
+
+    // Let user confirm using lowercase characters
+    lowChar = confirm("Click OK to confirm including lowercase character.");
+
+    if (lowChar == false) {
+        while (lowChar == false) {
+            lowChar = confirm(
+                "Click OK to confirm including lowercase characters."
+            );
+        }
+    }
+    // console.log(lowChar);
+
+    // Let user confirm using uppercase characters
+    uppChar = confirm("Click OK to confirm including uppercase character.");
+
+    if (uppChar == false) {
+        while (uppChar == false) {
+            uppChar = confirm(
+                "Click OK to confirm including uppercase characters."
+            );
+        }
+    }
+    // console.log(uppChar);
+
+    // Possible password values
+    let values =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+0123456789abcdefghijklmnopqrstuvwxyz";
+
+    // Initialization
+    let generatedPassword = "";
+
+    // create for loop to choose password characters
+    for (let i = 0; i < pwdLength; i++) {
+        generatedPassword += values.charAt(
+            Math.floor(Math.random() * Math.floor(values.length - 1))
+        );
+    }
+
+    alert(generatedPassword);
+
+    // add generatedPassword to textbox
+    document.getElementById("placeholder").value = generatedPassword;
+}
